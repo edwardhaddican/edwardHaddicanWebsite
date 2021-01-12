f01e1a474d484b03b70103b906b8598892a7e35e
Eddie Haddican <edwardhaddican@gmail.com>
2021-01-12 16:29:16 -0500

    edidted nav bar, title, and some css for contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,25 +1,27 @@
 import React from "react";
 import { Layout } from "../components/common";
+// Styles
+import "../styles/app.css";
 
 const ContactMe = () => {
     return (
         <Layout>
-            <div>
+            <div className="contact-main-container">
                 <h1>Contact Me</h1>
                 <p>Email: edwardhaddican@gmail.com</p>
                 <p>Cell: 845-642-9834</p>
-                <div>
-                    <p>LinkedIn: </p>
+                <p>
+                    <span>LinkedIn: </span>
                     <a href="https://www.linkedin.com/in/edhaddican/">
                         https://www.linkedin.com/in/edhaddican/
                     </a>
-                </div>
-                <div>
-                    <p>GitHub: </p>
+                </p>
+                <p>
+                    <span>GitHub: </span>
                     <a href="https://github.com/edwardhaddican">
                         https://github.com/edwardhaddican
                     </a>
-                </div>
+                </p>
             </div>
         </Layout>
     );
---
+++ src/pages/lectures.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Layout } from "../components/common";
+
+const Lectures = () => {
+    return (
+        <Layout>
+            <div>
+                <h1>Lectures</h1>
+            </div>
+        </Layout>
+    );
+};
+
+export default Lectures;
